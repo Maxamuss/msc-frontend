3470c035ea6750fb30fd15dc49a7cd3ae792a401
Max Wilkinson <maxwilkinson99@gmail.com>
2022-06-30 11:08:07 +0100

    basic

--- src/developer/core/TopMenu.tsx
+++ src/developer/core/TopMenu.tsx
@@ -1,10 +1,11 @@
 import { Fragment } from 'react';
 import { Menu, Transition } from '@headlessui/react';
 import { BellIcon, CloudUploadIcon, LightningBoltIcon, MenuAlt2Icon } from '@heroicons/react/outline';
-import { SearchIcon } from '@heroicons/react/solid';
 import { Link } from 'react-router-dom';
+
 import { ROUTES } from '../utils/routing';
 import { publishRelease } from '../releases/api';
+import ReleaseOverview from '../releases/ReleaseOverview';
 
 export default function TopMenu() {
 
@@ -23,26 +24,9 @@ export default function TopMenu() {
                 <MenuAlt2Icon className='h-6 w-6' aria-hidden='true' />
             </button>
             <div className='flex-1 px-4 flex justify-between'>
-                <div className='flex-1 flex'>
-                    <form className='w-full flex md:ml-0' action='#' method='GET'>
-                        <label htmlFor='search-field' className='sr-only'>
-                            Search
-                        </label>
-                        <div className='relative w-full text-gray-400 focus-within:text-gray-600'>
-                            <div className='absolute inset-y-0 left-0 flex items-center pointer-events-none'>
-                                <SearchIcon className='h-5 w-5' aria-hidden='true' />
-                            </div>
-                            <input
-                                id='search-field'
-                                className='block w-full h-full pl-8 pr-3 py-2 border-transparent text-gray-900 placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-0 focus:border-transparent sm:text-sm'
-                                placeholder='Search'
-                                type='search'
-                                name='search'
-                            />
-                        </div>
-                    </form>
-                </div>
-                <div className='ml-4 flex items-center md:ml-6'>
+                <div className='flex-1 flex'></div>
+                <div className='mr-4 ml-4 flex items-center md:ml-6'>
+                    <ReleaseOverview />
                     <button
                         type='button'
                         onClick={handlePublishRelease}
---
+++ src/developer/releases/ReleaseOverview.tsx
@@ -0,0 +1,7 @@
+export default function ReleaseOverview() {
+    return (
+        <div className='w-64 h-32 border-gray-200 text-black'>
+            1 change
+        </div>
+    )
+}
